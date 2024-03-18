e04bf64965c925ecc41d03c01b59bbe143439d7b
amanafro <amanauelisaak15@gmail.com>
2024-03-18 15:42:00 +0100

    added a mysql db connection

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var balance float32
+type Account struct {
+	balance float32
+}
 
-func main() {
+func intro(acc *Account) {
 
 	fmt.Printf("welcome to your account, what do you want? \n 1. cash in \n 2. cash out \n 3. get balance \n")
 	choice := ""
@@ -15,38 +19,51 @@ func main() {
 
 	switch choice {
 	case "1":
-		cashIn()
+		cashIn(acc)
 	case "2":
-		cashOut()
+		cashOut(acc)
 	case "3":
-		getBalance()
+		getBalance(acc)
 	default:
 		fmt.Println("invalid choice (1, 2, 3)")
 	}
-
 }
 
-func getBalance() float32 {
-	return balance
+func main() {
+
+	var account Account
+
+	for {
+
+		intro(&account)
+		quit := ""
+		fmt.Println("(X) for quit, (Enter) for continue")
+		fmt.Scanln(&quit)
+		if quit == "x" {
+			break
+		}
+	}
 }
 
-func cashIn() {
+func getBalance(acc *Account) {
+	fmt.Println("your current balance is:", acc.balance)
+}
 
+func cashIn(acc *Account) {
 	var newBalance float32
-
 	fmt.Println("How much money u wanna deposit? ")
 	fmt.Scanln(&newBalance)
 
 	if newBalance < 5 {
 		fmt.Print("you can't desposit less than $5")
 	} else {
-		balance += newBalance
+		acc.balance += newBalance
 		fmt.Println("your money was desposited successfully!")
-		fmt.Println("your current balance is:", newBalance)
+		fmt.Println("your current balance is:", acc.balance)
 	}
 }
 
-func cashOut() {
+func cashOut(acc *Account) {
 	var lessBalance float32
 
 	fmt.Println("how much do you want to withdraw")
@@ -55,6 +72,7 @@ func cashOut() {
 	if lessBalance < 2 {
 		fmt.Println("you can't withdraw less than $2")
 	} else {
-		balance -= lessBalance
+		acc.balance -= lessBalance
 	}
+	fmt.Println("your current balance is:", acc.balance)
 }
